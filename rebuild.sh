3fc3a0923e23c96e1b32c1ce80a9ea8f7474981e
leon dowling <leon.dowling@integrationventure.com>
2025-09-10 11:10:06 -0500

    Add rebuild script to manage Docker Compose lifecycle

---
+++ rebuild.sh
@@ -0,0 +1,5 @@
+
+
+docker compose -f docker-compose-localai.yml -f docker-compose-localai-override.private.yml down
+
+docker compose -f docker-compose-localai.yml -f docker-compose-localai-override.private.yml up -d
